6948363a1a08819737d259629e960aa9cca3296d
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-06-27 23:21:26 +0300

    Add Stock card component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
     <b-navbar class="navbar" toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand to="/">NavBar</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/">Home</b-nav-item>
           <b-nav-item to="Portfolio">Portfolio</b-nav-item>
           <b-nav-item to="Stocks">Stocks</b-nav-item>
         </b-navbar-nav>
@@ -15,7 +14,7 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-button variant="warning">End Day</b-button>
-          <b-nav-item-dropdown text="Save & Load" right>
+          <b-nav-item-dropdown class="dropdown" text="Save & Load" right>
             <b-dropdown-item href="#">Save Data</b-dropdown-item>
             <b-dropdown-item href="#">Load Data</b-dropdown-item>
           </b-nav-item-dropdown>
@@ -27,7 +26,10 @@
 
 <style lang="scss" scoped>
 .navbar {
-    margin-bottom: 3rem;
-    border-radius: 5px;
+  margin-bottom: 3rem;
+  border-radius: 5px;
+}
+.dropdown {
+  margin-left: 1rem;
 }
 </style>
